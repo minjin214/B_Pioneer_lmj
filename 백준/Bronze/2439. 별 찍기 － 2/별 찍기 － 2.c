055536d42927dc9207b97461a20b575034239318
minjin214 <168632991+minjin214@users.noreply.github.com>
2024-10-02 19:17:16 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void)
+{
+    int N;
+    int i=0,j=0;
+
+    scanf("%d",&N);
+    while(i<N)
+    {
+        ++i;
+        while(j<N-i)
+        {
+            printf(" ");
+            ++j;
+        }
+        j=0;
+        while (j<i)
+        {
+            printf("*");
+            ++j;
+        }
+        j=0;
+        printf("\n");
+    }
+    return 0;
+}
