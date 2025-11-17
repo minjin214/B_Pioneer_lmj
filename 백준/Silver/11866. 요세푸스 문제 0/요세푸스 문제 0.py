222c8d6a613783b0fa40416129514dcebe81e497
minjin214 <168632991+minjin214@users.noreply.github.com>
2025-11-17 21:07:38 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+
+people = list(range(1, n + 1))
+result = []
+index = 0 
+
+while people:
+    index = (index + k - 1) % len(people)
+    
+    result.append(people.pop(index))
+    
+print('<' + ', '.join(map(str, result)) + '>')
